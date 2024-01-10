8bed73fe360f6c7a5061a7c63407356eb5c26492
Emran147 <emran.qedan11@hotmail.com>
2024-01-10 14:36:03 +0200

    added the mongodb connection

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/WeatherDB", {
+mongoose.connect("mongodb+srv://emranqedan11:E5b8037ec1@emranscluster0.p4bywbz.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
 }).catch((err) => console.log(err));
 
