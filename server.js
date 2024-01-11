8e4aebf26e9a740ec4f2bca164f3ab02fe03fe9a
Emran147 <emran.qedan11@hotmail.com>
2024-01-11 16:34:05 +0200

    changed the listen for poth servers

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://emranqedan11:E5b8037ec1@emranscluster0.p4bywbz.mongodb.net/WeatherApp?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://emranqedan11:E5b8037ec1@emranscluster0.p4bywbz.mongodb.net/WeatherApp?retryWrites=true&w=majority" || "mongodb://127.0.0.1:27017/WeatherDB", { useNewUrlParser: true })
 
 app.use('/', api)
 
-const port = 3000
-app.listen(port, function () {
-  console.log(`Running on port ${port}`);
-});
+const PORT = 8080
+app.listen(process.env.PORT || PORT)
